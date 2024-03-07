e9b72716ae691db47777d5c819ca5408e4df1220
John D <johnd@Johns-MBP.attlocal.net>
2024-03-06 18:43:05 -0600

    new clientside work

--- Develop/server/models/index.js
+++ Develop/server/models/index.js
@@ -5,23 +5,7 @@ module.exports = { User, Book };
 
 
 
-type Book = {
-  authors: [String]
-  description: String!
-  bookId: String!
-  image: String
-  link: String
-  title: String!
-}
 
-type User = {
-  _id: ID!
-  username: String!
-  email: String!
-  password: String!
-  savedBooks: [Book]!
-  bookCount: Int
-}
 
-const User = require('./path/to/User'); // Import User model
+
 
