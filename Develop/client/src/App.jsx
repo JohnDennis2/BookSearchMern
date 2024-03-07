74be2901e14a689045e1736fe4c31cddcbd982b8
John D <johnd@Johns-MBP.attlocal.net>
2024-03-06 20:40:14 -0600

    app Js work

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -5,12 +5,19 @@ import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  uri: '/',
+  cache: new InMemoryCache()
+});
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
